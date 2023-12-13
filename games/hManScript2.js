0abecabc6f075181234f978629e58cc7a39f7e21
Ian M. Nava Garduno <ianmng1@gmail.com>
2023-12-12 20:01:48 -0800

    Displaying in UI the secret word with underscores

--- games/hManScript2.js
+++ games/hManScript2.js
@@ -5,10 +5,10 @@
 // Buttons
 const generateWordBtn = document.querySelector(".generateBtn");
 // Containers
-const secretWordContainer = document.querySelector(".randomWordContainer");
+const randomWordContainer = document.querySelector(".randomWordContainer");
 const keyBoardContainer = document.querySelector(".keyBoard-container");
 
-// 1. Display consonants as buttons
+//// 1. Display consonants as buttons
 const consonants = [
   "b",
   "c",
@@ -43,14 +43,14 @@ consonants.forEach((el, i) => {
   const playWithButtons = function (index) {
     let buttons = keyBoardContainer.querySelectorAll(".keyButton");
     buttons[index].addEventListener("click", function () {
-      console.log(index);
+      console.log(consonants[index], index);
     });
   };
 
   playWithButtons(i);
 });
 
-// 2. Display random word using the game info
+//// 2. Display random word using the game info
 const gameInfo = {
   words: [
     "Developer",
@@ -76,12 +76,35 @@ const gameInfo = {
   currentWord: [],
 };
 
-// Get a random word from obj.words and spread the word into an array
+// Get a random word from gameInfo.words and spread the word into an array
 const getRandomWord = function (obj) {
   return (obj.currentWord = [
     ...obj.words[Math.trunc(Math.random() * obj.words.length)],
   ]);
 };
 
-console.log(getRandomWord(gameInfo));
-console.log(gameInfo.currentWord[0]);
+getRandomWord(gameInfo);
+const currentWord = gameInfo.currentWord;
+console.log(currentWord);
+
+// Cleaning container first
+randomWordContainer.innerHTML = "";
+
+// Replace consonants with underscores
+const replaceConsonants = currentWord.map((el) => {
+  for (let i = 0; i < consonants.length; i++) {
+    if (el.toUpperCase().includes(consonants[i].toUpperCase())) {
+      el = el.replace(new RegExp(consonants[i], "gi"), "_");
+    }
+  }
+  return el;
+});
+// console.log(replaceConsonants);
+
+// Exporting the current word to the HTML
+replaceConsonants.forEach((el, i) => {
+  // "el" is a string type
+  const secretWordHTML = `<span class="currentWord" id="${i}">${el.toUpperCase()}</span>`;
+
+  randomWordContainer.insertAdjacentHTML("beforeend", secretWordHTML);
+});
