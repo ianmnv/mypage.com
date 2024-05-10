ca8c4d5ed058122cd7ec90d1453d371d78ff9801
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-05-09 21:42:36 -0600

    Index/ Section 1: Markers on map can display correctly

--- index/script.js
+++ index/script.js
@@ -156,13 +156,14 @@ document.addEventListener("keydown", function (e) {
   e.key === "ArrowLeft" && previousSlide();
 });
 
-// MAP INFO
+//// MAP CLASS
 const locationCont = document.querySelector(".s1-slide-child");
 const citysInfo = {
   0: [
     `Mexico City is by far my favorite city because here my whole childhood happen, my family is here and 90% of my friends, 
   also in Mexico City you will never get bored since there's a lot to do here and because you'll find amazing food very easy 
   but since it's a very popular city it can also be chaotic because through time a lot of people have arrived.`,
+    [[19.3753973, -99.1366977]],
   ],
   1: [
     `1. I find Quintana Roo top-notch for having beautiful beaches, 
@@ -173,6 +174,12 @@ const citysInfo = {
   for the delicious food you can find here and the friendly people same as in all places in Mexico (almost 😬). <br> <br>
   4. In Jalisco, we have Puerto Vallarta and Manzanillo for having great weather, 
   great beaches as well and fun atmosphere and of course great food as well.`,
+    [
+      [21.1213783, -86.9388046],
+      [15.8748973, -97.0976379],
+      [19.0400289, -98.2745829],
+      [20.6408555, -105.2655067],
+    ],
   ],
   2: [
     `Vancouver is just an amazing, modern, clean, filled with nature city, I liked every day living there, 
@@ -182,6 +189,7 @@ const citysInfo = {
   probably many people will disagree with me but that's my personal opinion
   on Van, I'm not saying it's not amazing but in countries there's always that is something missing even in Mexico; 
   loved the parks, the city but since I come from a huge city, Vancouver was kinda empty for me lol.`,
+    [[49.2577062, -123.2063038]],
   ],
   3: [
     `1. Whistler 😍: Small town but with an incredible views from the top of the mountians, 
@@ -190,6 +198,11 @@ const citysInfo = {
   and mostly in summer there are a lot of festivals and events in that area which makes it perfect to socialize. <br> <br>
   3. Victoria is also another super small city but very beautiful everywhere you walk or go, mostly all Vancouver island is very pretty. 
   There are also very good restaurants but the one that showed up the most was 'Milestones', kinda miss that restaurant.`,
+    [
+      [50.1041188, -123.0839148],
+      [49.3151148, -123.0909845],
+      [48.4262073, -123.379822],
+    ],
   ],
 };
 
@@ -209,6 +222,7 @@ class Map {
     this._getLocation();
   }
 
+  // MAP INFO
   _displayMapInfo() {
     locationCont.innerHTML = `
     <h1 class='map-title'>LOCATIONS:</h1>
@@ -275,10 +289,49 @@ class Map {
     const cityInfos = `<p>${this.citiesInfo[dataCity][0]}</p>`;
     mapDiv.innerHTML = cityInfos;
 
-    // addMap([19.3753973, -99.1366977]);
+    // Display marker on map
+
+    // cdmx
+    // [19.3753973, -99.1366977]
+
+    // Quintana Roo, Oaxaca, Puebla, Puerto Vallarta
+    // [21.1213783,-86.9388046], [15.8748973,-97.0976379], [19.0400289,-98.2745829], [20.6408555,-105.2655067]
+
+    // Vancouver
+    // [49.2577062,-123.2063038]
+
+    // Whistler, North Vancouver, Victoria
+    // [50.1041188,-123.0839148], [49.3151148,-123.0909845], [48.4262073,-123.379822]
+
+    const lats = [];
+    const lngs = [];
+
+    this.citiesInfo[dataCity][1].forEach((arr, i) => {
+      lats.push(arr[0]);
+      lngs.push(arr[1]);
+
+      const [lat, lng] = arr;
+      this.addMarker([lat, lng], i + 1 + "");
+    });
+
+    const maxLat = Math.max(...lats);
+    const maxLng = Math.max(...lngs);
+    const minLat = Math.min(...lats);
+    const minLng = Math.min(...lngs);
+
+    this.#map
+      .fitBounds(
+        [
+          [maxLat, maxLng],
+          [minLat, minLng],
+        ],
+        { padding: [100, 100] }
+      )
+      .setZoom(6);
   }
 
-  ////// MAP
+  ////// MAP ITSELF
+  // Reverse geocoding `https://geocode.xyz/${lat},${lng}?geoit=json`
 
   _getLocation() {
     navigator.geolocation.getCurrentPosition(
@@ -301,16 +354,19 @@ class Map {
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(this.#map);
-
-    // addMap(coords);
   }
 
-  addMap(c) {
-    // [19.3753973, -99.1366977]
-
-    L.marker(c)
-      .addTo(map)
-      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+  addMarker(coords, msg) {
+    L.marker(coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          minWidth: 10,
+          autoClose: false,
+          className: "map-popup",
+        })
+      )
+      .setPopupContent(msg)
       .openPopup();
   }
 }
